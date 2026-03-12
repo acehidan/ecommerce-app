e986ead72b468c36e06c38aed9662854af0a802b
acehidan <aungaungoogame2@gmail.com>
2026-03-12 14:24:18 +0630

    fix err message toast

--- app/chat.jsx
+++ app/chat.jsx
@@ -330,7 +330,7 @@ export default function Chat() {
       Toast.show({
         type: 'error',
         text1: 'Error',
-        text2: error.response.data.message,
+        text2: "Failed to send image",
       });
     } finally {
       setSending(false);
